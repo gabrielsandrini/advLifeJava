70da071d3eff2b861a90ff4b4a308bf82a1bbdd5
Gabriel Albinati Sandrini <gabriel.sandrini@outlook.com>
2018-09-09 21:23:51 -0300

    implementação da sessão e da proteção de páginas

---
+++ src/controle/filters/LoginFilter.java
@@ -0,0 +1,59 @@
+package controle.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter(servletNames= {"Faces Servlet"})
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		if (session.getAttribute("nickname")!=null || req.getRequestURI().endsWith("Login.xhtml") ||
+			req.getRequestURI().endsWith("CadastroUsuario.xhtml")) {
+			
+			chain.doFilter(request, response);
+		} else {
+			HttpServletResponse res = (HttpServletResponse) response;
+			res.sendRedirect("Login.xhtml");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
